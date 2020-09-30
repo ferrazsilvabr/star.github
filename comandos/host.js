6af729ec4d15506694acceae019893c00ee2ced7
yADGithub <2007adrequigames@gmail.com>
2020-09-30 16:27:26 +0000

    testes '-'

--- comandos/host.js
+++
@@ -1,5 +0,0 @@
-exports.run = (client, message, args) => {
-
-message.channel.send(`Estou Consumindo: **${(process.cpuUsage().system / 1024 / 1024).toFixed(2)}%** de CPU`)
-message.channel.send(`Estou Consumindo **${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)}MB de 500MB** de Memória RAM`)
-}
